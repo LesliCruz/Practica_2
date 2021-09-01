df8f3745f1c92f7f93747d79deb4601f5134aa15
Lesli Cruz <Leslicruz_23@hotmail.com>
2021-08-31 19:14:12 -0700

    Agregar archivos de proyecto.

---
+++ CalculoPerimetro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoPerimetro
+{
+    class Program
+    {
+
+        //En la solucion con el nombre Practica_2, agregar un proyecto de tipo consola con el nombre de
+        //CalculoPerimetro.
+        //Declarar las variables necesarias para mostrar en pantalla el calculo del perimeto del triangulo
+        //Mostrar en pantalla el valor del perimetro, con el siguiente mensaje "El perimetro del triangulo es: "
+        static void Main(string[] args)
+        {
+            //(paso 1)
+            int a = 10;
+            int b = 10;
+            int c = 7;
+            //( paso2)
+            int p = a + b + c;
+            //(paso 3)
+            Console.WriteLine("El perimetro del triangulo es:" + p);
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_Int/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Int
+{
+    class Program
+    {
+        //Para comentar CONTROL + K + C para descomentar CONTROL + K + U
+        //Paso 1. Crear 3 variables de tipo INT y asignarles valores esteticos
+        //Paso 2. Crear una variable de tipo int la cual guardara la suma de las 3 variables anteriores
+        //Paso 3. Mostrar en pantalla el valor de SUMA, con el siguiente mensaje "El valor de SUMA es: " <suma>
+        static void Main(string[] args)
+        {
+            //(paso 1)
+            int numero_1 = 5;
+            int numero_2 = 10;
+            int numero_3 = 20;
+           //( paso2)
+            int suma = numero_1 + numero_2 + numero_3;
+           //(paso 3)
+            Console.WriteLine("El valor de la suma es:" + suma);
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
