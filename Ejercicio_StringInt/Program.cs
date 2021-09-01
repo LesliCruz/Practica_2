1f773b52969a0d1c9d84db3d98fb6f0c57c11603
Lesli Cruz <Leslicruz_23@hotmail.com>
2021-08-31 19:59:27 -0700

    Se agrego la tarea StringInt

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        //Pedir en la consola el nombre y edad
+        //Mostrar en pantalla el siguiente mensaje "Te llamas + nombre y tienes + edad años"
+        static void Main(string[] args)
+        {
+            int
+            String nombre;
+            String edad;
+            Console.WriteLine("Escribe tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Escribe tu edad");
+            edad = Console.ReadLine();
+            Console.WriteLine("Te llamas " + nombre + " y tienes " + edad + " años ");
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
